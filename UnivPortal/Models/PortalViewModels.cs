b3b6d42f3f6dade1b70839360bb7322939d4c7b9
tnoumess <noume55i@yahoo.fr>
2015-02-10 00:44:53 -0800

    First phase of Models finished

--- UnivPortal/Models/PortalViewModels.cs
+++ UnivPortal/Models/PortalViewModels.cs
@@ -14,37 +14,107 @@ namespace UnivPortal.Models
     }
     public enum Role
     {
-        S, I, A
+       N, S, I, A
     }
     public class Student
     {
+        [Display(Name = "StudentID")]
         [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [RegularExpression(@"/^[G]{1}[0-9]{6}$/", ErrorMessage = "Bad pattern! Try G followed by 6 digit.")]
         public int StudentID { get; set; }
+
+        [Display(Name = "Last Name")]
+        [RegularExpression(@"^[A-Z][-'a-zA-Z]{2,15}$", ErrorMessage = "Bad pattern! Alphabetic only and between 2 and 15 characters.")]
         public string LastName { get; set; }
+
+        [Display(Name = "First Name")]
+        [RegularExpression(@"^[A-Z][-'a-zA-Z]{2,15}$", ErrorMessage = "Bad pattern! Alphabetic only and between 2 and 15 characters.")]
         public string FirstName { get; set; }
+
+        [Display(Name = "Date of Birth")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yy}", ApplyFormatInEditMode = true)]
         public DateTime DateofBirth { get; set; }
+
+        [Display(Name = "Major ID")]
+        [RegularExpression(@"^[A-Z]{2,5}$", ErrorMessage = "Bad pattern! Please contact administrator.")]
         public String MajorID { get; set; }
+
+        [Display(Name = "Email")]
+        [RegularExpression(@"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}",ErrorMessage="Invalid email address format.")]
         public String Email { get; set; }
 
+        [Display(Name = "Full Name")]
+        public string FullName
+        {
+            get
+            {
+                return LastName + ", " + FirstName;
+            }
+        }
+
         public virtual ICollection<Enrollment> Enrollments { get; set; }
+        public virtual Major Majors { get; set; }
     }
+
+
+
     public class Instructor
     {
+        [Display(Name = "Instructor ID")]
         [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [RegularExpression(@"/^[G]{1}[0-9]{6}$/", ErrorMessage = "Bad pattern! Try G followed by 6 digits.")]
         public int InstrutorID { get; set; }
+
+        [Display(Name = "Last Name")]
+        [RegularExpression(@"^[A-Z][-'a-zA-Z]{2,15}$", ErrorMessage = "Bad pattern! Alphabetic only and between 2 and 15 characters.")]
         public string LastName { get; set; }
+
+        [Display(Name = "Last Name")]
+        [RegularExpression(@"^[A-Z][-'a-zA-Z]{2,15}$", ErrorMessage = "Bad pattern! Alphabetic only and between 2 and 15 characters.")]
         public string FirstName { get; set; }
+
+        [Display(Name = "Date of Birth")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yy}", ApplyFormatInEditMode = true)]
         public DateTime DateofBirth { get; set; }
-        public String DepartmentID { get; set; }
+
+        [Display(Name = "Email")]
+        [RegularExpression(@"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}", ErrorMessage = "Invalid email address format.")]
         public String Email { get; set; }
+
+        [Display(Name = "Full Name")]
+        public string FullName
+        {
+            get
+            {
+                return LastName + ", " + FirstName;
+            }
+        }
+
+        public virtual ICollection<Department> Departments { get; set; }
     }
     public class Course
     {
         [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [Display(Name = "Course ID")]
+        [RegularExpression(@"^[A-Z]{2,5}[0-9]{3}$", ErrorMessage = "Bad pattern! 2 to 5 upper Char followed by 3 digits.")]
         public String CourseID { get; set; }
+
+        [Display(Name = "Course Name")]
+        [RegularExpression(@"^[\w]{1}[-'\s\w]{2,30}+$", ErrorMessage = "Bad pattern! Character between 2 and 30.  char, - and ' allowed only")]
         public String Name { get; set; }
+
+        [Display(Name = "Course Description")]
+        [RegularExpression(@"^[\w]{1}[-'\s\w]{2,30}+$", ErrorMessage = "Bad pattern! Alphabetic only and between 2 and 50 characters.")]
         public String Description { get; set; }
-        public int DepartmentID { get; set; }
+
+        [Display(Name = "Department")]
+        [RegularExpression(@"/^[A-Z]{2,5}$/", ErrorMessage = "Bad pattern! Try [2 -5] Caracters UPPER Case.")]
+        public String DepartmentID { get; set; }
+
+        [Display(Name = "Number of Credits")]
+        [Range(0, 3)]
         public int Credit { get; set; }
 
         public virtual Department Department { get; set; }
@@ -55,8 +125,10 @@ namespace UnivPortal.Models
     public class Enrollment
     {
         public int EnrollmentID { get; set; }
-        public int CourseID { get; set; }
-        public int StudentID { get; set; }
+        public String CourseID { get; set; }
+        public String StudentID { get; set; }
+
+        [DisplayFormat(NullDisplayText = "No grade")]
         public Grade? Grade { get; set; }
 
         public virtual Course Course { get; set; }
@@ -65,16 +137,29 @@ namespace UnivPortal.Models
     public class Department
     {
         [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [Display(Name = "Department ID")]
+        [RegularExpression(@"^[A-Z]{2,5}$", ErrorMessage = "Bad pattern! 2 to 5 upper Char.")]
         public String DepartmentID { get; set; }
+
+        [Display(Name = "Name")]
+        [RegularExpression(@"^[\w]{1}[-'\s\w]{2,15}+$", ErrorMessage = "Bad pattern! Character between 2 and 15.  char, - and ' allowed only")]
         public String Name { get; set; }
+
+        public int? InstructorID { get; set; }
+
         public virtual Instructor Administrator { get; set; }
         public virtual ICollection<Major> Majors { get; set; }
 
     }
     public class Major
     {
+        [Display(Name = "Major ID")]
         [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [RegularExpression(@"^[A-Z]{2,5}$", ErrorMessage = "Bad pattern! Pattern SWE, CS, ISA, IT ...")]
         public String MajorID { get; set; }
+
+        [Display(Name = "Name")]
+        [RegularExpression(@"^[\w]{1}[-'\s\w]{2,15}+$", ErrorMessage = "Bad pattern! Character between 2 and 15.  char, - and ' allowed only")]
         public String Name { get; set; }
         public virtual ICollection<Course> Listcourses { get; set; }
     }
